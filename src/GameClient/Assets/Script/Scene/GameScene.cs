ad708b371d5213fc3b9c4fdb8f8301bbacc23f3a
Esun Kim <veblush@gmail.com>
2015-10-27 18:05:33 +0900

    UI for Login #1

--- src/GameClient/Assets/Script/Scene/GameScene.cs
+++ src/GameClient/Assets/Script/Scene/GameScene.cs
@@ -6,60 +6,13 @@ using Domain.Interfaced;
 
 public class GameScene : MonoBehaviour
 {
-    private UserRef _user;
-    private string _userId;
-
-    void Start()
+    protected void Start()
     {
-        ApplicationComponent.TryInit();
-
-        G.Comm = new Communicator(G.Logger, this);
-        G.Comm.ServerEndPoint = new IPEndPoint(IPAddress.Loopback, 9001);
-        G.Comm.Start();
-
-        StartCoroutine(ProcessLoginUser());
     }
 
-    private IEnumerator ProcessLoginUser()
+    public void OnLeaveButtonClick()
     {
-        Debug.LogFormat("ProcessLoginUser");
-
-        var userLogin = new UserLoginRef(new SlimActorRef { Id = 1 }, new SlimRequestWaiter { Communicator = G.Comm }, null);
-
-        // Login
-
-        var t1 = userLogin.Login("tester", "1234", G.Comm.IssueObserverId());
-        yield return t1.WaitHandle;
-        ShowResult(t1, "Login");
-        if (t1.Exception != null)
-            yield break;
-        _user = new UserRef(new SlimActorRef { Id = t1.Result }, new SlimRequestWaiter { Communicator = G.Comm }, null);
-
-        // Get UserId (just for test)
-
-        var t2 = _user.GetId();
-        yield return t2.WaitHandle;
-        ShowResult(t2, "GetId");
-        _userId = t2.Result;
-    }
-
-    void ShowResult(Task task, string name)
-    {
-        if (task.Status == TaskStatus.RanToCompletion)
-            Debug.Log(string.Format("{0}: Done", name));
-        else if (task.Status == TaskStatus.Faulted)
-            Debug.Log(string.Format("{0}: Exception = {1}", name, task.Exception));
-        else if (task.Status == TaskStatus.Canceled)
-            Debug.Log(string.Format("{0}: Canceled", name));
-        else
-            Debug.Log(string.Format("{0}: Illegal Status = {1}", name, task.Status));
-    }
-
-    void ShowResult<TResult>(Task<TResult> task, string name)
-    {
-        if (task.Status == TaskStatus.RanToCompletion)
-            Debug.Log(string.Format("{0}: Result = {1}", name, task.Result));
-        else
-            ShowResult((Task)task, name);
+        // TODO: Confirmation Dialog
+        Application.LoadLevel("MainScene");
     }
 }
---
+++ src/GameClient/Assets/Script/Scene/MainScene.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using Akka.Interfaced;
+using Domain.Interfaced;
+using DG;
+using DG.Tweening;
+
+public class MainScene : MonoBehaviour
+{
+    public RectTransform LoginPanel;
+    public RectTransform MainPanel;
+
+    private UserRef _user;
+    private string _userId;
+
+    void Start()
+    {
+        ApplicationComponent.TryInit();
+
+        G.Comm = new Communicator(G.Logger, this);
+        G.Comm.ServerEndPoint = new IPEndPoint(IPAddress.Loopback, 9001);
+        G.Comm.Start();
+
+        StartCoroutine(ProcessLoginUser());
+
+        LoginPanel.gameObject.SetActive(true);
+        MainPanel.gameObject.SetActive(false);
+    }
+
+    public void OnLoginButtonClick()
+    {
+        var y = LoginPanel.anchoredPosition.y;
+        LoginPanel.anchoredPosition = new Vector2(0, y);
+        LoginPanel.DOAnchorPos(new Vector2(-700, y), 0.3f)
+                  .OnComplete(() => LoginPanel.gameObject.SetActive(false));
+
+        MainPanel.gameObject.SetActive(true);
+        MainPanel.anchoredPosition = new Vector2(700, y);
+        MainPanel.DOAnchorPos(new Vector2(0, y), 0.3f);
+    }
+
+    public void OnPlayButtonClick()
+    {
+        // TODO: Check connected
+        Application.LoadLevel("GameScene");
+    }
+
+    public void OnSpectateButtonClick()
+    {
+    }
+
+    private IEnumerator ProcessLoginUser()
+    {
+        Debug.LogFormat("ProcessLoginUser");
+
+        var userLogin = new UserLoginRef(new SlimActorRef { Id = 1 }, new SlimRequestWaiter { Communicator = G.Comm }, null);
+
+        // Login
+
+        var t1 = userLogin.Login("tester", "1234", G.Comm.IssueObserverId());
+        yield return t1.WaitHandle;
+        ShowResult(t1, "Login");
+        if (t1.Exception != null)
+            yield break;
+        _user = new UserRef(new SlimActorRef { Id = t1.Result }, new SlimRequestWaiter { Communicator = G.Comm }, null);
+
+        // Get UserId (just for test)
+
+        var t2 = _user.GetId();
+        yield return t2.WaitHandle;
+        ShowResult(t2, "GetId");
+        _userId = t2.Result;
+    }
+
+    void ShowResult(Task task, string name)
+    {
+        if (task.Status == TaskStatus.RanToCompletion)
+            Debug.Log(string.Format("{0}: Done", name));
+        else if (task.Status == TaskStatus.Faulted)
+            Debug.Log(string.Format("{0}: Exception = {1}", name, task.Exception));
+        else if (task.Status == TaskStatus.Canceled)
+            Debug.Log(string.Format("{0}: Canceled", name));
+        else
+            Debug.Log(string.Format("{0}: Illegal Status = {1}", name, task.Status));
+    }
+
+    void ShowResult<TResult>(Task<TResult> task, string name)
+    {
+        if (task.Status == TaskStatus.RanToCompletion)
+            Debug.Log(string.Format("{0}: Result = {1}", name, task.Result));
+        else
+            ShowResult((Task)task, name);
+    }
+}
